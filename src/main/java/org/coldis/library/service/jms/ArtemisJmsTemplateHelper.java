ea149ce00a550bf05cab72eb66cc514f7ccf2dbe
rvcoutinho <me@rvcoutinho.com>
2023-11-09 14:27:04 -0300

    - Fixing delays;
    Signed-off-by: rvcoutinho <me@rvcoutinho.com>

--- src/main/java/org/coldis/library/service/jms/ArtemisJmsTemplateHelper.java
+++ src/main/java/org/coldis/library/service/jms/ArtemisJmsTemplateHelper.java
@@ -24,6 +24,28 @@ public class ArtemisJmsTemplateHelper implements JmsTemplateHelper {
 	private static final Random RANDOM = new Random();
 
 	/**
+	 * Gets the current millis.
+	 *
+	 * @return Current millis.
+	 */
+	private Long getCurrentMillis() {
+		// System.out.println(DateTimeHelper.getCurrentLocalDateTime().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
+		// System.out.println(
+		// DateTimeHelper.getCurrentLocalDateTime().atZone(ZoneId.systemDefault()).withZoneSameInstant(ZoneId.of("UTC")).toInstant().toEpochMilli());
+		// System.out.println(System.currentTimeMillis());
+		// final LocalDateTime now = DateTimeHelper.getCurrentLocalDateTime();
+		// final long timestampWithTimezone =
+		// now.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli()
+		// + (ZoneId.systemDefault().getRules().getOffset(now).getTotalSeconds() * 1000
+		// * -1);
+		// System.out.println(timestampWithTimezone);
+
+		return DateTimeHelper.getCurrentLocalDateTime().atZone(ZoneId.systemDefault()).withZoneSameInstant(ZoneId.of("UTC")).toInstant().toEpochMilli();
+		// return System.currentTimeMillis();
+		// return timestampWithTimezone;
+	}
+
+	/**
 	 * @see org.coldis.library.service.jms.JmsTemplateHelper#send(org.springframework.jms.core.JmsTemplate,
 	 *      org.coldis.library.service.jms.JmsMessage)
 	 */
@@ -36,10 +58,13 @@ public class ArtemisJmsTemplateHelper implements JmsTemplateHelper {
 			// Creates the message.
 			final javax.jms.Message jmsMessage = template.getMessageConverter().toMessage(message.getMessage(), session);
 			if ((message.getFixedDelay() > 0) || (message.getRandomDelay() > 0)) {
-				jmsMessage.setLongProperty(Message.HDR_SCHEDULED_DELIVERY_TIME.toString(), DateTimeHelper.getCurrentLocalDateTime()
-						.atZone(ZoneId.systemDefault()).withZoneSameInstant(ZoneId.of("UTC")).toInstant().toEpochMilli()
-						+ (message.getFixedDelay()
-								+ (message.getRandomDelay() <= 0 ? 0 : Math.abs(ArtemisJmsTemplateHelper.RANDOM.nextInt(message.getRandomDelay())) * 1000)));
+				final long scheduledTimestamp = System.currentTimeMillis() + ((message.getFixedDelay()
+
+						+ (message.getRandomDelay() <= 0 ? 0 : Math.abs(ArtemisJmsTemplateHelper.RANDOM.nextInt(message.getRandomDelay())))));
+				jmsMessage.setLongProperty(Message.HDR_SCHEDULED_DELIVERY_TIME.toString(), scheduledTimestamp);
+				System.out.println(System.currentTimeMillis());
+				System.out.println(scheduledTimestamp);
+
 			}
 			// Sets the priority.
 			if (message.getPriority() != null) {
--- src/test/java/org/coldis/library/test/service/client/JmsTemplateServiceTest.java
+++ src/test/java/org/coldis/library/test/service/client/JmsTemplateServiceTest.java
@@ -1,7 +1,8 @@
 package org.coldis.library.test.service.client;
 
+import java.util.Random;
+
 import javax.jms.JMSException;
-import javax.jms.Message;
 
 import org.coldis.library.test.TestHelper;
 import org.junit.jupiter.api.Assertions;
@@ -18,6 +19,11 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 public class JmsTemplateServiceTest {
 
 	/**
+	 * Random.
+	 */
+	private static final Random RANDOM = new Random();
+
+	/**
 	 * Test service.
 	 */
 	@Autowired
@@ -30,14 +36,9 @@ public class JmsTemplateServiceTest {
 	 */
 	@BeforeEach
 	public void clear() throws JMSException {
+
 		// Clears acked messages.
 		JmsTemplateTestService.ACKED_MESSAGES.clear();
-
-		// Makes sure there are no messages to consume before the test.
-		Message message = this.jmsTemplateTestService.consumeMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE, 100L);
-		while (message != null) {
-			message = this.jmsTemplateTestService.consumeMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE, 100L);
-		}
 
 	}
 
@@ -49,28 +50,31 @@ public class JmsTemplateServiceTest {
 	@Test
 	public void testLastValue() throws Exception {
 
+		final Integer random = JmsTemplateServiceTest.RANDOM.nextInt();
+
 		// Sends messages in a row.
-		this.jmsTemplateTestService.sendMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE, "message1" /* message */, null /* last value */, 0, 0);
-		this.jmsTemplateTestService.sendMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE, "message2", null, 0, 0);
-		this.jmsTemplateTestService.sendMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE, "message3", "teste2", 0, 0);
-		this.jmsTemplateTestService.sendMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE, "message4", "teste2", 0, 0);
+		this.jmsTemplateTestService.sendMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE + random, "message1-" + random /* message */,
+				null /* last value */, 0, 0);
+		this.jmsTemplateTestService.sendMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE + random, "message2-" + random, null, 0, 0);
+		this.jmsTemplateTestService.sendMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE + random, "message3-" + random, "teste2", 0, 0);
+		this.jmsTemplateTestService.sendMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE + random, "message4-" + random, "teste2", 0, 0);
 
 		// Wait until all messages are sent and consumes them.
-		this.jmsTemplateTestService.consumeMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE, 1000L);
-		this.jmsTemplateTestService.consumeMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE, 1000L);
-		this.jmsTemplateTestService.consumeMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE, 1000L);
-		this.jmsTemplateTestService.consumeMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE, 1000L);
+		this.jmsTemplateTestService.consumeMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE + random, 1000L);
+		this.jmsTemplateTestService.consumeMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE + random, 1000L);
+		this.jmsTemplateTestService.consumeMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE + random, 1000L);
+		this.jmsTemplateTestService.consumeMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE + random, 1000L);
 
 		// Last values messages should not be consumed twice,
-		Assertions.assertTrue(JmsTemplateTestService.ACKED_MESSAGES.contains("message1"));
-		Assertions.assertTrue(JmsTemplateTestService.ACKED_MESSAGES.contains("message2"));
-		Assertions.assertTrue(!JmsTemplateTestService.ACKED_MESSAGES.contains("message3"));
-		Assertions.assertTrue(JmsTemplateTestService.ACKED_MESSAGES.contains("message4"));
+		Assertions.assertTrue(JmsTemplateTestService.ACKED_MESSAGES.contains("message1-" + random));
+		Assertions.assertTrue(JmsTemplateTestService.ACKED_MESSAGES.contains("message2-" + random));
+		Assertions.assertTrue(!JmsTemplateTestService.ACKED_MESSAGES.contains("message3-" + random));
+		Assertions.assertTrue(JmsTemplateTestService.ACKED_MESSAGES.contains("message4-" + random));
 
 		// A new last value message should be consumed.
-		this.jmsTemplateTestService.sendMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE, "message5", "teste2", 0, 0);
-		this.jmsTemplateTestService.consumeMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE, 1000L);
-		Assertions.assertTrue(JmsTemplateTestService.ACKED_MESSAGES.contains("message5"));
+		this.jmsTemplateTestService.sendMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE + random, "message5-" + random, "teste2", 0, 0);
+		this.jmsTemplateTestService.consumeMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE + random, 1000L);
+		Assertions.assertTrue(JmsTemplateTestService.ACKED_MESSAGES.contains("message5-" + random));
 	}
 
 	/**
@@ -81,30 +85,36 @@ public class JmsTemplateServiceTest {
 	@Test
 	public void testScheduled() throws Exception {
 
+		final Integer random = JmsTemplateServiceTest.RANDOM.nextInt();
+
 		// Sends messages in a row.
-		Assertions.assertFalse(JmsTemplateTestService.ACKED_MESSAGES.contains("message1"));
-		this.jmsTemplateTestService.sendMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE, "message1" /* message */, null /* last value */,
-				30 /* fixed wait */, 0 /* random wait */);
-		this.jmsTemplateTestService.consumeMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE, 1L);
-		Assertions.assertFalse(JmsTemplateTestService.ACKED_MESSAGES.contains("message1"));
+		Assertions.assertFalse(JmsTemplateTestService.ACKED_MESSAGES.contains("testScheduled1-" + random));
+		this.jmsTemplateTestService.sendMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE + random, "testScheduled1-" + random /* message */,
+				null /* last value */, 30 * 1000 /* fixed wait */, 0 /* random wait */);
+		this.jmsTemplateTestService.consumeMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE + random, 10L);
+		Assertions.assertFalse(JmsTemplateTestService.ACKED_MESSAGES.contains("testScheduled1-" + random));
 
 		TestHelper.waitUntilValid(() -> {
-			return JmsTemplateTestService.ACKED_MESSAGES.contains("message1");
+			return JmsTemplateTestService.ACKED_MESSAGES.contains("testScheduled1-" + random);
 		}, consumed -> {
 			try {
-				this.jmsTemplateTestService.consumeMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE, 1L);
+				this.jmsTemplateTestService.consumeMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE + random, 10L);
+				this.jmsTemplateTestService.consumeMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE + random, 10L);
+				this.jmsTemplateTestService.consumeMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE + random, 10L);
 			}
 			catch (final JMSException expcetion) {
 				throw new RuntimeException();
 			}
 			return consumed != null;
-		}, 25 * 1000, 1 * 1000);
-		Assertions.assertFalse(JmsTemplateTestService.ACKED_MESSAGES.contains("message1"));
+		}, 27 * 1000, 1 * 1000);
+		Assertions.assertFalse(JmsTemplateTestService.ACKED_MESSAGES.contains("testScheduled1-" + random));
 
 		// Waits and tries again.
-		Thread.sleep(5 * 1000);
-		this.jmsTemplateTestService.consumeMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE, 1L);
-		Assertions.assertTrue(JmsTemplateTestService.ACKED_MESSAGES.contains("message1"));
+		Thread.sleep(30 * 1000);
+		this.jmsTemplateTestService.consumeMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE + random, 10L);
+		this.jmsTemplateTestService.consumeMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE + random, 10L);
+		this.jmsTemplateTestService.consumeMessage(JmsTemplateTestService.JMS_TEMPLATE_TEST_QUEUE + random, 10L);
+		Assertions.assertTrue(JmsTemplateTestService.ACKED_MESSAGES.contains("testScheduled1-" + random));
 
 	}
 
